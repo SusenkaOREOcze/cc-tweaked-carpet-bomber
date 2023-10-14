6e4b7542725f6c6a9bf600eaff3439565074081a
SusenkaOREOcz <humorousgmsvidea@gmail.com>
2023-10-14 18:13:33 +0200

    edit in extraction files

--- git.lua
+++ git.lua
@@ -1,28 +1,12 @@
-local function copyFolder(sourcePath, destPath)
-    -- create the destination directory if it does not exist
-    if not fs.exists(destPath) then
-        fs.makeDir(destPath)
-    end
-
-    -- list all items in the source directory
-    local items = fs.list(sourcePath)
-    for _, item in ipairs(items) do
-        local fullSourcePath = fs.combine(sourcePath, item)
-        local fullDestPath = fs.combine(destPath, item)
-
-        if fs.isDir(fullSourcePath) then
-            -- if the item is a directory, copy it recursively
-            copyFolder(fullSourcePath, fullDestPath)
-        else
-            -- if the item is a file, copy it
-            fs.copy(fullSourcePath, fullDestPath)
-        end
-    end
-end
-
 print("Extracting files to root")
 print("------------------------------------------")
-copyFolder("cc-tweaked-carpet-bomber", "/")
+shell.run("cc-tweaked-carpet-bomber/TURTLE", ".")
+shell.run("cc-tweaked-carpet-bomber/POCKET", ".")
+shell.run("cc-tweaked-carpet-bomber/HUB", ".")
+
+shell.run("cc-tweaked-carpet-bomber/turtles.lua", ".")
+shell.run("cc-tweaked-carpet-bomber/pockets.lua", ".")
+
 shell.run("delete cc-tweaked-carpet-bomber/")
 print("------------------------------------------")
 print("Done")
--- turtles.lua
+++ turtles.lua
@@ -10,10 +10,10 @@ print("Copying TURTLE/ to all turtles and pockets")
 print("------------------------------------------")
 for i, value in ipairs(turtles) do
     if fs.exists(value .. "/TURTLE") then
-        shell.run("delete " .. "../" .. "/TURTLE")
+        shell.run("delete " .. value .. "/TURTLE")
     end
-    shell.run("copy TURTLE/ " .. "../" .. value)
-    print("Copying TURTLE/ to " .. "../" .. value)
+    shell.run("copy TURTLE/ " .. value)
+    print("Copying TURTLE/ to " .. value)
 end
 print("------------------------------------------")
 print("Done")
