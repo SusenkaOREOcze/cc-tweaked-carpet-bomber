4fd4a8234880bd157f57cd28f6dcdee1348e8701
SusenkaOREOcz <humorousgmsvidea@gmail.com>
2023-10-14 16:44:49 +0200

    added extract file

--- extract.lua
+++ extract.lua
@@ -10,26 +10,38 @@ local pockets = {
     "disk6",
 }
 
+local turtles_tab = shell.openTab("turtles")
+local pockets_tab = shell.openTab("pockets")
+local git = shell.openTab("git")
+
+shell.switchTab(git)
 print("Extracting files to root")
+print("------------------------------------------")
 shell.run("copy cc-tweaked-carpet-bomber/ .")
 shell.run("delete cc-tweaked-carpet-bomber/")
-print("Done")
 print("------------------------------------------")
-print(" ")
+print("Done")
+
+shell.switchTab(turtles_tab)
 print("Copying TURTLE/ to all turtles and pockets")
 print("------------------------------------------")
 for i, value in ipairs(turtles) do
-    shell.run("delete " .. value .. "/TURTLE")
+    if fs.exists(value .. "/TURTLE") then
+        shell.run("delete " .. value .. "/TURTLE")
+    end
     shell.run("copy TURTLE/ " .. value)
-
     print("Copying TURTLE/ to " .. value)
 end
 print("------------------------------------------")
-print(" ")
+print("Done")
+
+shell.switchTab(pockets_tab)
 print("Copying TURTLE/ to all pockets")
 print("------------------------------------------")
 for i, value in ipairs(pockets) do
-    shell.run("delete " .. value .. "/POCKET")
+    if fs.exists(value .. "/POCKET") then
+        shell.run("delete " .. value .. "/POCKET")
+    end
     shell.run("copy POCKET/ " .. value)
 
     print("Copying POCKET/ to " .. value)
