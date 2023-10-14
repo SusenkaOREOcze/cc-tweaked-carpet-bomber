d4ee6019988dc5b31b0cb60a1b0956bc70c6df8b
SusenkaOREOcz <humorousgmsvidea@gmail.com>
2023-10-14 19:58:32 +0200

    changed traversal script + more guards etc..

--- TURTLE/turtle.lua
+++ TURTLE/turtle.lua
@@ -10,6 +10,8 @@ local await_fuel = false
 local designated_id = 0
 local calibrated = true
 
+local traversal_x = false
+local traversal_z = false
 
 local hub_direction = "west"
 local turtle_direction = "east"
@@ -139,129 +141,156 @@ while start_rise do
 end
 
 -- get to target
-print(target.direction)
 while move_to_target do
     -- print("Moving to target...")
     
     local x, y, z = gps.locate()
+    print("Current position: " .. x .. " " .. y .. " " .. z)
+    print("---")
 
     -- based on target direction
     -- move respectivly on x and z
 
     --target is west
     if target.direction == "west" then
+        traversal_z = true
+
         if target.x == x and target.z == z then
             move_to_target = false
             align_before_bombing = true
+
+            traversal_x = false
+            traversal_z = false
         end
 
-        if z < target.z and turtle_direction == 'east' then
+        if z == target.x and turtle_direction == 'east' then
             turtle.turnRight()
             turtle_direction = 'south'
-        elseif z > target.z and turtle_direction == 'east' then
+
+            traversal_x = true
+            traversal_z = false
+        end
+
+        if turtle_direction == 'east' and traversal_z then
             turtle.turnRight()
-            turtle_direction = 'south'
-        elseif (z < target.z) and turtle_direction == 'south' then
+            turtle_direction = 'west'
+        end
+        if z < target.z and turtle_direction == 'south' and traversal_z then
             turtle.forward()
-        elseif (z > target.z) and turtle_direction == 'south' then
+        elseif z > target.z and turtle_direction == 'south' and traversal_z then
             turtle.back()
         end
 
-        if x < target.x and turtle_direction == 'south' then
-            turtle.turnRight()
-            turtle_direction = 'west'
-        elseif x > target.x and turtle_direction == 'south' then
-            turtle.turnRight()
-            turtle_direction = 'west'
-        elseif (x < target.x) and turtle_direction == 'west' then
-            turtle.forward()
-        elseif (x > target.x) and turtle_direction == 'west' then
+
+        if x < target.x and turtle_direction == 'west' and traversal_x then
             turtle.back()
+        elseif x > target.x and turtle_direction == 'west' and traversal_x then
+            turtle.forward()
         end
     end
 
 
     -- target is east
     if target.direction == "east" then
+        traversal_z = true
+
         if target.x == x and target.z == z then
             move_to_target = false
             align_before_bombing = true
+
+            traversal_x = false
+            traversal_z = false
         end
 
-        if z < target.z and turtle_direction == 'east' then
+        if z == target.x and turtle_direction == 'east' then
+            turtle.turnLeft()
+            turtle_direction = 'east'
+
+            traversal_x = true
+            traversal_z = false
+        end
+
+        if turtle_direction == 'east' and traversal_z then
             turtle.turnRight()
             turtle_direction = 'south'
-        elseif z > target.z and turtle_direction == 'east' then
-            turtle.turnRight()
-            turtle_direction = 'south'
-        elseif (z < target.z) and turtle_direction == 'south' then
+        end
+        if z < target.z and turtle_direction == 'south' and traversal_z then
             turtle.forward()
-        elseif (z > target.z) and turtle_direction == 'south' then
+        elseif z > target.z and turtle_direction == 'south' and traversal_z then
             turtle.back()
         end
 
-        if x < target.x and turtle_direction == 'south' then
-            turtle.turnLeft()
-            turtle_direction = 'east'
-        elseif x > target.x and turtle_direction == 'north' then
-            turtle.turnRight()
-            turtle_direction = 'east'
-        elseif (x < target.x) and turtle_direction == 'east' then
+        if x < target.x and turtle_direction == 'east' and traversal_x then
             turtle.forward()
-        elseif (x > target.x) and turtle_direction == 'east' then
-            turtle.forward()
+        elseif x > target.x and turtle_direction == 'east' and traversal_x then
+            turtle.bback()
         end
     end
 
 
     -- target is south
     if target.direction == "south" then
+
+        traversal_x = true
         if target.x == x and target.z == z then
             move_to_target = false
             align_before_bombing = true
+
+            traversal_x = false
+            traversal_z = false
         end
 
-        if x < target.x and turtle_direction == 'east' then
+        if x == target.x and turtle_direction == 'east' then
+            turtle.turnRight()
+            turtle_direction = 'south'
+
+            traversal_x = false
+            traversal_z = true
+        end
+
+        if x < target.x and turtle_direction == 'east' and traversal_x then
             turtle.forward()
-        elseif x > target.x and turtle_direction == 'east' then
+        elseif x > target.x and turtle_direction == 'east' and traversal_x then
             turtlle.back()
         end
 
-        if z < target.z and turtle_direction == 'east' then
-            turtle.turnRight()
-            turtle_direction = 'south'
-        elseif z > target.z and turtle_direction == 'east' then
-            turtle.turnRight()
-            turtle_direction = 'south'
-        elseif (z < target.z) and turtle_direction == 'south' then
-            turtle.forward()
-        elseif (z > target.z) and turtle_direction == 'south' then
+
+        if z < target.z and turtle_direction == 'south' and traversal_z then
             turtle.back()
+        elseif z > target.z and turtle_direction == 'south' and traversal_z then
+            turtle.forward()
         end
     end
 
     -- target is north
     if target.direction == "north" then
+
+        traversal_x = true
         if target.x == x and target.z == z then
             move_to_target = false
             align_before_bombing = true
+
+            traversal_x = false
+            traversal_z = false
+        end
+        
+        if x == target.x and turtle_direction == 'east' then
+            turtle.turnLeft()
+            turtle_direction = 'north'
+
+            traversal_x = false
+            traversal_z = true
         end
 
-        if x < target.x and turtle_direction == 'east' then
+        if x < target.x and turtle_direction == 'east' and traversal_x then
             turtle.forward()
-        elseif x > target.x and turtle_direction == 'east' then
+        elseif x > target.x and turtle_direction == 'east' and traversal_x then
             turtlle.back()
         end
 
-        if z < target.z and turtle_direction == 'east' then
-            turtle.turnLeft()
-            turtle_direction = 'north'
-        elseif z > target.z and turtle_direction == 'east' then
-            turtle.turnLeft()
-            turtle_direction = 'north'
-        elseif (z < target.z) and turtle_direction == 'north' then
+        if z < target.z and turtle_direction == 'north' and traversal_z then
             turtle.forward()
-        elseif (z > target.z) and turtle_direction == 'north' then
+        elseif z > target.z and turtle_direction == 'north' and traversal_z then
             turtle.back()
         end
     end
