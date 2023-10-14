bb58b8c7a3d462ceefc5ce27ab1fc185e5d6cf29
SusenkaOREOcz <humorousgmsvidea@gmail.com>
2023-10-14 18:02:05 +0200

    edit in extract

--- git.lua
+++ git.lua
@@ -1,6 +1,28 @@
+local function copyFolder(sourcePath, destPath)
+    -- create the destination directory if it does not exist
+    if not fs.exists(destPath) then
+        fs.makeDir(destPath)
+    end
+
+    -- list all items in the source directory
+    local items = fs.list(sourcePath)
+    for _, item in ipairs(items) do
+        local fullSourcePath = fs.combine(sourcePath, item)
+        local fullDestPath = fs.combine(destPath, item)
+
+        if fs.isDir(fullSourcePath) then
+            -- if the item is a directory, copy it recursively
+            copyFolder(fullSourcePath, fullDestPath)
+        else
+            -- if the item is a file, copy it
+            fs.copy(fullSourcePath, fullDestPath)
+        end
+    end
+end
+
 print("Extracting files to root")
 print("------------------------------------------")
-shell.run("copy . ../")
+copyFolder("cc-tweaked-carpet-bomber", "/")
 shell.run("delete cc-tweaked-carpet-bomber/")
 print("------------------------------------------")
 print("Done")
