ae0288688d9646e5f3858df1c71bb41fbdfe343a
SusenkaOREOcz <humorousgmsvidea@gmail.com>
2023-10-14 17:14:26 +0200

    edit in extract.lua

--- extract.lua
+++ extract.lua
@@ -1,6 +1,6 @@
-local turtles_tab = multishell.launch({}, "cc-tweaked-carpet-bomber/turtles.lua")
-local pockets_tab = multishell.launch({}, "cc-tweaked-carpet-bomber/pockets.lua")
-local git = multishell.launch({}, "cc-tweaked-carpet-bomber/git.lua")
+local turtles_tab = multishell.launch({fs = fs, shell = shell}, "cc-tweaked-carpet-bomber/turtles.lua")
+local pockets_tab = multishell.launch({fs = fs, shell = shell}, "cc-tweaked-carpet-bomber/pockets.lua")
+local git = multishell.launch({shell = shell}, "cc-tweaked-carpet-bomber/git.lua")
 
 multishell.setTitle(turtles_tab, "Turtles")
 multishell.setTitle(pockets_tab, "Pockets")
